fd472c820533386f0252312b6c1906aa4e327931
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-02 18:47:04 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1 
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine($"Q1 - Second price: {prices[1]}");
         }
     }
 }
